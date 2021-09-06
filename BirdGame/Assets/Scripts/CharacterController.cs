c64d6fbadf5b6ea2d64eb91b0543adcd40ab8a96
Alessia <alessia@mipsscan.com>
2021-09-06 15:15:13 -0400

    Player Character Animation

--- BirdGame/Assets/Scripts/CharacterController.cs
+++ BirdGame/Assets/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
@@ -17,52 +18,75 @@ public class CharacterController : MonoBehaviour
     public GameObject leftWingIn;
     public GameObject leftWingJump;
     public GameObject leftWingDown;
+
+    public bool canjump = true;
     
-    
+    public GameObject wowBackground;
     
     public bool inRange = false;
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) && 
-            this.transform.position.x > leftBound.transform.position.x )
-        {
-            var newpos = new Vector2(this.transform.position.x  - (1.5f * Time.deltaTime), 
-                                    this.transform.position.y);
-            this.transform.position = newpos;
-            
-            wingsWalkLeft();
-            
-        } else if (Input.GetKey(KeyCode.RightArrow) && 
-              this.transform.position.x < rightBound.transform.position.x )
-        {
-            var newpos = new Vector2(this.transform.position.x + (1.5f * Time.deltaTime), 
-                                    this.transform.position.y );
-            this.transform.position = newpos;
-            
-            wingsWalkRight();
 
-        }
-        else
+        if (canjump == true)
         {
-            standing();
+            
+            
+            
+            if (Input.GetKey(KeyCode.LeftArrow) && 
+                this.transform.position.x > leftBound.transform.position.x )
+            {
+                var newpos = new Vector2(this.transform.position.x  - (1.5f * Time.deltaTime), 
+                    this.transform.position.y);
+                this.transform.position = newpos;
+                Debug.Log(("left"));
+                wingsWalkLeft();
+            
+            } else if (Input.GetKey(KeyCode.RightArrow) && 
+                       this.transform.position.x < rightBound.transform.position.x )
+            {
+                var newpos = new Vector2(this.transform.position.x + (1.5f * Time.deltaTime), 
+                    this.transform.position.y );
+                this.transform.position = newpos;
+            
+                wingsWalkRight();
+
+            } else if (Input.GetKeyDown(KeyCode.Space) )
+            {
+                canjump = false;
+                jump();
+            }
+            else 
+            {
+                standing();
+            }
+            
+            
         }
+       
+        
+        
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        wowBackground.SetActive(true);
         inRange = true;
         Debug.Log(other.gameObject.name);
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
+        wowBackground.SetActive(true);
         inRange = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        wowBackground.SetActive(false);
         inRange = false;
     }
 
@@ -104,7 +128,11 @@ public class CharacterController : MonoBehaviour
         rightWingOut.SetActive(false);
         rightWingJump.SetActive(false);
     }
-    
+
+    void jump()
+    {
+        this.gameObject.GetComponent<Animator>().Play("PlayerJump");
+    }
     
 
 }
