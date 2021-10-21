4cc196aaf178a291b35c78cf9dc04fdc7a729fcf
Alessia Ianni-Palarchio <alessia@mipsscan.com>
2021-10-20 20:47:24 -0400

    Updated Line Renderers to be More Visible

---
+++ BirdGame/Assets/Scripts/PrototypeThree/SavePlayerInputs.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePlayerInputs : MonoBehaviour
+{
+    //This is for saving real-time inputs
+
+    public List<int> PlayerKeyInputs;
+    //checking against ints is slightly more efficient than checking against a string, for the future
+
+    public List<OnBeat.timing> playerTiming;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+        } 
+    }
+}
