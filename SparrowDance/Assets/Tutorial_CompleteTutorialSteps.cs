71a80562b646d5bd97040476e1b8bb57fdaff227
Alessia Ianni-Palarchio <alessia@mipsscan.com>
2022-04-27 11:42:54 -0400

    Tutorial Updates

--- SparrowDance/Assets/Tutorial_CompleteTutorialSteps.cs
+++ SparrowDance/Assets/Tutorial_CompleteTutorialSteps.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Tutorial_CompleteTutorialSteps : MonoBehaviour
 {
+    
+    public PlayerInput InputActions;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InputActions.currentActionMap.actions[1].performed += SpontaneousInput;
+        InputActions.currentActionMap.actions[2].performed += CalmInput;
+        InputActions.currentActionMap.actions[3].performed += PassionateInput;
+        InputActions.currentActionMap.actions[4].performed += EncouragingInput;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpontaneousInput(InputAction.CallbackContext obj)
     {
-        
+        Debug.Log("Spontaneous");
     }
+    
+    private void CalmInput(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Calm");
+    }
+    
+    private void PassionateInput(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Passionate");
+    }
+    
+    private void EncouragingInput(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Encouraging");
+    }
+    
 }
